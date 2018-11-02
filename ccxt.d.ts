b66f4fff31ea615f0ba6340ec5af3945ece9fa45
Manuel Haller Polo <manuel@wondrous.ch>
2018-11-02 17:51:06 +0100

    move not working dex to waitlist

--- ccxt.d.ts
+++ ccxt.d.ts
@@ -401,6 +401,7 @@ declare module 'ccxt' {
     export class huobicny extends huobipro {}
     export class huobipro extends Exchange {}
     export class ice3x extends Exchange {}
+    export class idex extends Exchange {}
     export class independentreserve extends Exchange {}
     export class indodax extends Exchange {}
     export class itbit extends Exchange {}
